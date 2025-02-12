c6439d40e2eadd8e108e9e99013ee4881dac63c9
Luis Gutiérrez <luisghtz@outlook.com>
2025-02-12 17:25:36 -0600

    Import config, openai and maths modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MathsModule } from './modules/maths/maths.module';
+import { OpenaiModule } from './modules/openai/openai.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), OpenaiModule, MathsModule],
   controllers: [],
   providers: [],
 })
