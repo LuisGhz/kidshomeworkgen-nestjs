5d7c66b11cbcdf26adfdfb03d3a1f01be75b5dcd
Luis Gutiérrez <luisghtz@outlook.com>
2025-02-12 17:25:56 -0600

    Create openai module and service

---
+++ src/modules/openai/openai.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { OpenAIService } from './services/openai.service';
+
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [OpenAIService],
+  exports: [OpenAIService],
+})
+export class OpenaiModule {}
---
+++ src/modules/openai/services/openai.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@nestjs/common';
+import OpenAI from 'openai';
+import {
+  ChatCompletionMessageParam,
+  ChatModel,
+  ResponseFormatJSONSchema,
+} from 'openai/resources';
+
+@Injectable()
+export class OpenAIService {
+  private readonly apiKey: string = process.env.OPENAI_API_KEY!;
+  private readonly model: ChatModel = 'gpt-4o-mini';
+  private readonly client = new OpenAI({
+    apiKey: this.apiKey,
+  });
+
+  async createCompletionWithJsonSchmea(
+    messages: ChatCompletionMessageParam[],
+    json_schema: ResponseFormatJSONSchema & { name: string },
+  ) {
+    return await this.client.chat.completions.create({
+      model: this.model,
+      messages,
+      response_format: {
+        type: 'json_schema',
+        json_schema: json_schema,
+        // json_schema: {
+        //   name: 'chat_response',
+        //   description: 'A chat response',
+        //   strict: true,
+        //   schema: {
+        //     type: 'object',
+        //     properties: {
+        //       message: {
+        //         type: 'string',
+        //       },
+        //     },
+        //   },
+        // },
+      },
+    });
+  }
+}
