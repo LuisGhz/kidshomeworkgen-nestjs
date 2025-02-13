3b22a6d3784f077eb5195a60fe80f303768c25e7
Luis Gutiérrez <luisghtz@outlook.com>
2025-02-13 12:33:46 -0600

    Set module as global

--- src/modules/openai/openai.module.ts
+++ src/modules/openai/openai.module.ts
@@ -1,6 +1,7 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { OpenAIService } from './services/openai.service';
 
+@Global()
 @Module({
   imports: [],
   controllers: [],
