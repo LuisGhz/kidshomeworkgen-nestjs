069d5804cc6c8cb50b2458be7f9300c826d34112
Luis Gutiérrez <luisghtz@outlook.com>
2025-02-14 11:08:33 -0600

    Add path to test templates

--- src/modules/maths/controllers/basic-maths.controller.ts
+++ src/modules/maths/controllers/basic-maths.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Res } from '@nestjs/common';
 import { BasicMathsService } from '../services/basic-maths.service';
 import { GenerateAdditionsDto } from '../dtos/generate-additions.dto';
 import { GenerateSubstractionsDto } from '../dtos/generate-substractions.dto';
 import { GenerateMultiplicationsDto } from '../dtos/generate-multiplications.dto';
 import { GenerateDivisionsDto } from '../dtos/generate-divisions.dto';
+import { Response } from 'express';
+import { compileTemplateToHtml } from 'src/shared/utils/compileTemplateToHtml';
+import { generatePDFFromHTML } from 'src/shared/utils/generatePDFFromHTML';
 
 @Controller('api/basic-maths')
 export class BasicMathsController {
@@ -41,5 +44,15 @@ export class BasicMathsController {
     const response =
       await this.basicMathsService.divisions(generateDivitionsDto);
     return response;
+  }
+
+  @Get('test-template')
+  async getTemp(@Res() res: Response) {
+    const html = compileTemplateToHtml('/maths/hbs/basic-maths/temp');
+    const pdf = await generatePDFFromHTML(html);
+    res.setHeader('Content-Type', 'application/pdf');
+    // res.setHeader('Content-Disposition', 'attachment; filename=maths.pdf');
+    res.setHeader('Content-Length', pdf.length);
+    res.send(Buffer.from(pdf));
   }
 }
