623704a3dab9d9812024f586e5e60daf152015ed
Luis Gutiérrez <luisghtz@outlook.com>
2025-02-16 10:01:03 -0600

    Add arithmetic

--- src/modules/maths/maths.module.ts
+++ src/modules/maths/maths.module.ts
@@ -3,10 +3,15 @@ import { BasicMathsController } from './controllers/basic-maths.controller';
 import { FractionsController } from './controllers/fractions.controller';
 import { BasicMathsService } from './services/basic-maths.service';
 import { FractionsService } from './services/fractions.service';
+import { ArithmeticController } from './controllers/arithmetic.controller';
 
 @Module({
   imports: [],
-  controllers: [BasicMathsController, FractionsController],
+  controllers: [
+    BasicMathsController,
+    FractionsController,
+    ArithmeticController,
+  ],
   providers: [BasicMathsService, FractionsService],
   exports: [],
 })
