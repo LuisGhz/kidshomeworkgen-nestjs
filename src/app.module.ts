5b9293ccb40eaae70529daef818468129750487d
Luis Gutiérrez <luisghtz@outlook.com>
2025-02-20 18:49:50 -0600

    Set config module props

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import appConfig from './config/app.config';
 import { MathsModule } from './modules/maths/maths.module';
 import { OpenaiModule } from './modules/openai/openai.module';
 import { SharedModule } from './modules/shared/shared.module';
+import appJoi from './config/app.joi';
 
 @Module({
-  imports: [ConfigModule.forRoot(), SharedModule, OpenaiModule, MathsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig],
+      validationSchema: appJoi,
+    }),
+    SharedModule,
+    OpenaiModule,
+    MathsModule,
+  ],
   controllers: [],
   providers: [],
 })
