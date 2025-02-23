d06a235bd35e0bc868c5d37cb89dc76c74167d8f
Luis Gutiérrez <luisghtz@outlook.com>
2025-02-23 12:34:35 -0600

    Remove unused config prop

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -4,7 +4,6 @@ interface ConfigProps {
   port: number;
   nodeEnv: string;
   openaiApiKey: string;
-  openaiApiUrl: string;
   pdfApiUri: string;
 }
 
@@ -14,7 +13,6 @@ export default registerAs(
     port: parseInt(process.env.PORT!) || 3000,
     nodeEnv: process.env.NODE_ENV || 'development',
     openaiApiKey: process.env.OPENAI_API_KEY!,
-    openaiApiUrl: process.env.OPENAI_API_URL!,
     pdfApiUri: process.env.PDF_API_URI!,
   }),
 );
