606713172447eed99b175e8fbb13f11a5e6a6242
Luis Gutiérrez <luisghtz@outlook.com>
2025-02-23 10:21:11 -0600

    Set model to arguments

--- src/modules/openai/services/openai.service.ts
+++ src/modules/openai/services/openai.service.ts
@@ -6,10 +6,11 @@ import {
   ResponseFormatJSONSchema,
 } from 'openai/resources';
 
+type AllowedChatModels = Extract<ChatModel, 'gpt-4o' | 'gpt-4o-mini'>;
+
 @Injectable()
 export class OpenAIService {
   private readonly apiKey: string = process.env.OPENAI_API_KEY!;
-  private readonly model: ChatModel = 'gpt-4o-mini';
   private readonly client = new OpenAI({
     apiKey: this.apiKey,
   });
@@ -25,9 +26,10 @@ export class OpenAIService {
   async createCompletionWithJsonSchema(
     messages: ChatCompletionMessageParam[],
     json_schema: ResponseFormatJSONSchema.JSONSchema,
+    model: AllowedChatModels = 'gpt-4o',
   ) {
     return await this.client.chat.completions.create({
-      model: this.model,
+      model,
       messages,
       response_format: {
         type: 'json_schema',
